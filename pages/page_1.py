f256b06c12b6a5a66a6b49f8e5587dbc39d728d2
Tudor Capusan <tudor@chucap.com>
2026-03-19 17:27:02 -0400

    clean up

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from src.package.extract_comments import extract_comments
+from src.package.render_comments import render_comments
 
-st.markdown("# Hello World")
+
+comments = extract_comments("/home/tudor/Downloads/documents/policy.docx")
+
+ALL_ELEMENTS = ["Comment", "Paragraph", "Sentence"]
+order = st.sidebar.multiselect(
+    "Elements to show", options=ALL_ELEMENTS, default=ALL_ELEMENTS
+)
+render_comments(*comments, order=order)
